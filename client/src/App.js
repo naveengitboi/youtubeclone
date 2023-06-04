5399a8e5a37f77ff45c506437dcdd360b51649bf
naveengitboi <naveenjangiti9@gmail.com>
2023-06-04 23:24:58 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import {Routes,Route} from 'react-router-dom'
+
+function App(){
+  return(
+    <>
+      <Routes>
+        <Route/>
+      </Routes>
+    </>
+  )
+}
+
+
+export default App
