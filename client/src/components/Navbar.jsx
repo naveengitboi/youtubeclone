24eb75abd00d41fc89985eff23077a806184a2e4
naveengitboi <naveenjangiti9@gmail.com>
2023-06-23 15:09:21 +0530

    changing navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import '../css/Navbar.css'
 function Navbar() {
   return (
     <div className="navbar">
+      
       <div className="logo">
          <h3 className='titles'><AiFillYoutube/>JustWatch</h3>
       </div>
