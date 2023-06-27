63974c423741fedeba6497679603de0ca99b4e54
naveengitboi <naveenjangiti9@gmail.com>
2023-06-27 17:21:04 +0530

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 import express from 'express'
 import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv'
 
+// dotenv 
+dotenv.config()
 
+//mongodb connection
+ 
+const connect = () => {
+    mongoose.connect(process.env.MONGODB).then(() => {
+        console.log('connected successfully').catch(() => {
+            console.log('not connected')
+        })  
+    })
+} 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
 app.listen(port, () => {
-    console.log(`server listening at port ${port}`)
+    connect();
+    console.log(`server listening at port ${port}`);
 });
