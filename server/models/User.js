72bafbe885ca3e0060f0208056e248a8ccfd887b
naveengitboi <naveenjangiti9@gmail.com>
2023-06-27 17:28:36 +0530

    user model

--- server/models/User.js
+++ server/models/User.js
@@ -5,5 +5,28 @@ const UserSchema = mongoose.Schema({
         type:String,
         unique: true,
         required: true
+    },
+    email:{
+        type : String,
+        unique: true,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    img:{
+        type: String
+
+    },
+    scubscibers: {
+        type : Number,
+        default : 0
+    },
+    subscribedUsers: {
+        type : [String]
     }
-})
+    
+} , {timestamps: true})
+
+export default mongoose.model('User', UserSchema)
