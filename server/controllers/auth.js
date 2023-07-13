376f07a18d2251d73c5d038625d9e4495695a85f
naveengitboi <naveenjangiti9@gmail.com>
2023-07-13 23:36:21 +0530

    added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 import User from '../models/User.js'
 export const signUp = async (req,res) => {
     try {
-        const newUser  = await new User()
+        const newUser   = await new User()
     } catch (error) {
-        console.log(error)
+        console.log(error) 
     }
 }
