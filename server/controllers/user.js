a681f94afa5ca5634d68f72376b598756cdbbff0
naveengitboi <naveenjangiti9@gmail.com>
2023-07-16 22:46:27 +0530

    added delete option

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,15 +3,29 @@ import { createError } from '../error.js'
 import User from '../models/User.js'
 // import mongoose from 'mongoose'
 
+
+//updat  user details
 export const update = async (req,res, next) => {
     const updateData = await User.findByIdAndUpdate({   "_id":req.params.id}, 
         {
             $set :req.body
         })
+    res.status(200).send("User details chaned successfully")
 }
-export const deleteUser= (req,res, next) => {
+
+//delete user from data base
+export const deleteUser= async (req,res, next) => {
+
+    await User.findByIdAndDelete(
+        {
+            "_id":req.params.id
+        }
+    )
+    res.status(200).send("User Deleted from database")
 
 }
+
+
 export const subscribe = (req,res, next)=>{
     
 }
