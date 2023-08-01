dcda649285b7ff854e4a4628a56cb57326038154
naveengitboi <naveenjangiti9@gmail.com>
2023-08-01 23:43:15 +0530

    added video controller

--- server/controllers/Video.js
+++ server/controllers/Video.js
@@ -1,14 +1,37 @@
 import express from 'express'
-
-
+import Video from '../models/Video.js'
+import { createError } from '../error.js'
 //add video
 export const addVideo = async (req,res,next) => {
-
+    const newVideo = new Video({userId : req.user.id, ...req.body})
+    try {
+        const saveVideo = await newVideo.save()
+        res.status(200).json(saveVideo)
+    } catch (error) {
+        next(error)
+        
+    }
 }
 
 //update video
 export const updateVideo = async (req,res,next) => {
-
+    try {
+        const video = Video.findById(req.params.id)
+        if (!video) {
+            return next(createError(404, "Video not found"))
+        }
+        if(req.user.id === video.userId){
+            const updateVideo = Video.findByIdAndUpdate(req.params.id, {
+                $set:req.body
+            }, {new:true})
+            res.status(200).json(updateVideo)
+        }
+        else{
+            next(createError(403, "You can only edit your video"))
+        }
+    } catch (error) {
+        next(error)
+    }
 }
 
 //deleteVideo
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { addVideo } from '../controllers/Video.js';
+import { addVideo, deleteVideo, getVideo, updateVideo } from '../controllers/Video.js';
+import { verify } from 'jsonwebtoken';
 
 
 const router = express.Router();
 
 router.post('/', addVideo)
-
+router.put('/:id', updateVideo)
+router.delete('/:id', verify, deleteVideo)
+router.get('/:id', getVideo)
 
 
 export default router
